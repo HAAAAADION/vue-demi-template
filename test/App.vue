02456822f81ef1e3ddc868ff06f87be111ab11e3
周浩辉 <fhlie11@qq.com>
2023-05-19 17:25:11 +0800

    feature, added test model

---
+++ test/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import HSelect from '@/components/select'
+import HInput from '@/components/input'
+import { HTable, HTableColumn } from '@/components/table'
+</script>
+
+<template>
+  <div>
+    <h-select :data="[]" />
+    <h-input />
+    <h-table>
+      <h-table-column></h-table-column>
+    </h-table>
+  </div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,37 +2,45 @@ import { defineConfig } from 'vite'
 import path, { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import ElementPlus from 'unplugin-element-plus/vite'
 
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'component',
-      fileName: 'index'
-      // formats: ['es']
+export default defineConfig(({ mode }) => {
+  const plugins = [vue(), vueJsx()]
+  if (mode !== 'production') {
+    plugins.push(ElementPlus())
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
     },
-    rollupOptions: {
-      external: ['vue', 'element-plus', 'async-validator']
-      // input: {
-      //   main: 'src/main.ts'
-      //   // main2: 'src/main.ts'
-      // }
-      // output: {
-      //   // // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-      //   // entryFileNames: '[name].js',
-      //   // // format: "es",
-      //   // inlineDynamicImports: false,
-      //   globals: {
-      //     vue: 'Vue',
-      //     vue2: 'Vue',
-      //   },
-      // },
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/main.ts'),
+        name: 'component',
+        fileName: 'index'
+        // formats: ['es']
+      },
+      rollupOptions: {
+        external: ['vue', 'element-plus', 'async-validator']
+        // input: {
+        //   main: 'src/main.ts'
+        //   // main2: 'src/main.ts'
+        // }
+        // output: {
+        //   // // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        //   // entryFileNames: '[name].js',
+        //   // // format: "es",
+        //   // inlineDynamicImports: false,
+        //   globals: {
+        //     vue: 'Vue',
+        //     vue2: 'Vue',
+        //   },
+        // },
+      }
     }
   }
 })
